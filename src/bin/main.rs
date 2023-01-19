fa270b9abb78086053b54451dcd5f7648d5af3aa
RobWalt <robwalter96@gmail.com>
2023-01-19 22:03:13 +0100

    feat(auth): implement login workflow

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,20 @@
-use clap::Parser;
-use cod_rs::MainArgs;
+use clap::{Command, FromArgMatches, Subcommand};
+use codeberg_cli_backend::{login, AuthArgs, MainArgs};
 
-fn main() {
-    let args = MainArgs::parse();
+#[tokio::main]
+async fn main() {
+    let cli = Command::new("cod");
+    let mut cli = MainArgs::augment_subcommands(cli);
+    let args = MainArgs::from_arg_matches(&cli.clone().get_matches()).unwrap();
+
+    let cmd_result = match args {
+        MainArgs::Auth(AuthArgs::Login(_login_args)) => login().await,
+    };
+
+    if let Err(error) = cmd_result {
+        println!("Error: {error:?}\n\n");
+        if let Err(e) = cli.print_long_help() {
+            println!("Couldn't print help message because: {e:?}");
+        }
+    }
 }
