99294703d570f49beac14c2e81d35ecae7000a8d
RobWalt <robwalter96@gmail.com>
2023-01-20 17:24:32 +0100

    feat(parse): `serde_json::Value` instead of `JSONResponse`

--- src/actions/user/info.rs
+++ src/actions/user/info.rs
@@ -5,7 +5,6 @@ use reqwest::Url;
 use crate::endpoints::{CODEBERG_API_BASE, USER_INFO};
 use crate::frontend::user::info::InfoArgs;
 use crate::types::client::CodebergClient;
-use crate::types::response::JSONResponse;
 use crate::types::token::Token;
 
 pub async fn info(args: InfoArgs, token: Token) -> anyhow::Result<()> {
@@ -17,7 +16,7 @@ pub async fn info(args: InfoArgs, token: Token) -> anyhow::Result<()> {
         .get(api_endpoint)
         .send()
         .await?
-        .json::<JSONResponse>()
+        .json::<serde_json::Value>()
         .await?;
 
     println!("{user_info:#?}");
