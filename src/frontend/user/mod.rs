109fbcdb2140bf69ad710afad10b3c2dfa85c1c4
RobWalt <robwalter96@gmail.com>
2023-01-20 17:01:58 +0100

    feat(userinfo): add new user info action

---
+++ src/frontend/user/mod.rs
@@ -0,0 +1,9 @@
+pub mod info;
+
+use clap::Subcommand;
+
+#[derive(Subcommand, Debug)]
+#[command(author, version, about = "User information", long_about = None)]
+pub enum UserArgs {
+    Info(info::InfoArgs),
+}
