4b1a3924423e065428daac23196abd5c8aa4c0f9
RobWalt <robwalter96@gmail.com>
2023-01-21 16:22:11 +0100

    feat(endpoints): implement endpoint generator

---
+++ src/endpoints/endpoint_generator.rs
@@ -0,0 +1,38 @@
+use std::str::FromStr;
+
+use reqwest::Url;
+
+use crate::endpoints::CODEBERG_API_BASE;
+
+#[derive(Debug, Clone, Copy)]
+pub struct EndpointGenerator;
+
+macro_rules! generator_method {
+    ($method_name:ident, $endpoint:ident) => {
+        impl EndpointGenerator {
+            pub fn $method_name() -> anyhow::Result<Url> {
+                use crate::endpoints::$endpoint;
+                let url = Url::from_str(CODEBERG_API_BASE)?.join($endpoint)?;
+                Ok(url)
+            }
+        }
+    };
+}
+
+generator_method!(verify, AUTHENTIFICATION_VERIFICATION);
+generator_method!(user_info, USER_INFO);
+generator_method!(user_followers, USER_FOLLOWERS);
+generator_method!(user_following, USER_FOLLOWING);
+generator_method!(user_repos, USER_REPOS);
+
+impl EndpointGenerator {
+    pub fn issues_list(owner: impl ToString, repo: impl ToString) -> anyhow::Result<Url> {
+        use crate::endpoints::{ISSUE_LIST_END, ISSUE_LIST_START};
+        let url = Url::from_str(CODEBERG_API_BASE)?
+            .join((ISSUE_LIST_START.to_owned() + "/").as_str())?
+            .join((owner.to_string() + "/").as_str())?
+            .join((repo.to_string() + "/").as_str())?
+            .join(ISSUE_LIST_END)?;
+        Ok(url)
+    }
+}
