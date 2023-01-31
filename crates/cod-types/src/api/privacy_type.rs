fd7c8a579047359fa89fa3e28ad5f2ad2b678f06
RobWalt <robwalter96@gmail.com>
2023-01-31 17:47:00 +0100

    feat(cli): use `ValueEnum` for visibility arguments

--- crates/cod-types/src/api/privacy_type.rs
+++ crates/cod-types/src/api/privacy_type.rs
@@ -1,6 +1,9 @@
+use clap::ValueEnum;
 use strum::{Display, EnumString, EnumVariantNames};
 
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Display, EnumVariantNames, EnumString)]
+#[derive(
+    Debug, Clone, Copy, Default, PartialEq, Eq, Display, EnumVariantNames, EnumString, ValueEnum,
+)]
 pub enum Privacy {
     #[default]
     Private,
