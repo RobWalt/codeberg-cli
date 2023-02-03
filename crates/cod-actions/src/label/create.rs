1d82a0af4ee54bc7eca9bcc0f0c076dd83cf4a28
RobWalt <robwalter96@gmail.com>
2023-02-03 17:07:31 +0100

    feat(label): add more cli args

--- crates/cod-actions/src/label/create.rs
+++ crates/cod-actions/src/label/create.rs
@@ -1,31 +1,98 @@
 use cod_cli::label::create::CreateLabelArgs;
 use cod_client::CodebergClient;
-use cod_render::spinner::spin_until_ready;
+use cod_render::ui::multi_fuzzy_select_with_key;
 use cod_types::api::create_label_options::CreateLabelOption;
-use reqwest::Url;
 
 use cod_endpoints::endpoint_generator::EndpointGenerator;
 use cod_types::api::label::Label;
+use strum::Display;
 
 pub async fn create_label(args: CreateLabelArgs, client: &CodebergClient) -> anyhow::Result<()> {
-    let label = spin_until_ready(async {
-        let api_endpoint = EndpointGenerator::repo_labels()?;
+    let options = fill_in_mandatory_values(&args)?;
+    let options = fill_in_optional_values(options, args)?;
 
-        create_label_post(client, args, api_endpoint).await
-    })
-    .await?;
+    let api_endpoint = EndpointGenerator::repo_labels()?;
+
+    let label: Label = client.post_body(api_endpoint, options).await?;
 
     println!("Successfully created label: {}", label.name);
 
     Ok(())
 }
 
-async fn create_label_post(
-    client: &CodebergClient,
+fn fill_in_mandatory_values(args: &CreateLabelArgs) -> anyhow::Result<CreateLabelOption> {
+    let name = match args.name.clone() {
+        Some(name) => name,
+        None => dialoguer::Input::new()
+            .with_prompt("Label Title")
+            .interact()?,
+    };
+    Ok(CreateLabelOption::new(name))
+}
+
+fn fill_in_optional_values(
+    mut options: CreateLabelOption,
     args: CreateLabelArgs,
-    api_endpoint: Url,
-) -> anyhow::Result<Label> {
-    let body = CreateLabelOption::new(args.name);
-    let response = client.post_body(api_endpoint, body).await?;
-    Ok(response)
+) -> anyhow::Result<CreateLabelOption> {
+    options = options
+        .with_color(args.color.clone().unwrap_or_default())
+        .with_description(args.description.clone().unwrap_or_default());
+
+    #[derive(Debug, Clone, Copy, Display, PartialEq, Eq)]
+    enum PossiblyMissing {
+        Description,
+        Color,
+    }
+
+    use PossiblyMissing::*;
+
+    let missing_options = [
+        args.color.is_none().then_some(Color),
+        args.description.is_none().then_some(Description),
+    ]
+    .into_iter()
+    .flatten()
+    .collect::<Vec<_>>();
+
+    if missing_options.is_empty() {
+        return Ok(options);
+    }
+
+    let selected_options = multi_fuzzy_select_with_key(
+        missing_options,
+        "Add additional information for",
+        |&missing_option| missing_option,
+        |missing_option| missing_option,
+        |_| false,
+    )?;
+
+    if selected_options.contains(&Description) {
+        let new_description = dialoguer::Editor::new()
+            .edit("Enter a label description")?
+            .ok_or_else(|| anyhow::anyhow!("Closed the editor. Aborting."))?;
+        options = options.with_description(new_description);
+    }
+
+    if selected_options.contains(&Color) {
+        let new_color = dialoguer::Input::new()
+            .with_prompt("Enter a color")
+            .validate_with(|color: &String| {
+                (color.len() == 7
+                    && color.starts_with('#')
+                    && color
+                        .chars()
+                        .skip(1)
+                        .take(6)
+                        .filter(|digit| digit.is_ascii_hexdigit())
+                        .count()
+                        == 6)
+                    .then_some(())
+                    .ok_or("Not a color: format <#XXXXXX>")
+            })
+            .interact()?;
+
+        options = options.with_color(new_color);
+    }
+
+    Ok(options)
 }
--- crates/cod-cli/src/label/create.rs
+++ crates/cod-cli/src/label/create.rs
@@ -4,6 +4,14 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct CreateLabelArgs {
     /// Label name
-    #[arg(short, long, required = true)]
-    pub name: String,
+    #[arg(short, long)]
+    pub name: Option<String>,
+
+    /// Label color (in hex format "#abcdef")
+    #[arg(short, long)]
+    pub color: Option<String>,
+
+    /// Label purpose description
+    #[arg(short, long)]
+    pub description: Option<String>,
 }
