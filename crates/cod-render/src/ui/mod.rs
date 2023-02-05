7277056dad5e733d8acc61cc1ffe26773188267b
RobWalt <robwalt@noreply.codeberg.org>
2023-02-05 11:34:53 +0000

    feat(actions): Implement default branch selection for `pull create` action (#48)
    Closes #44
    
    Authored-by: RobWalt <robwalter96@gmail.com>
    Reviewed-on: https://codeberg.org/RobWalt/codeberg-cli/pulls/48

--- crates/cod-render/src/ui/mod.rs
+++ crates/cod-render/src/ui/mod.rs
@@ -38,22 +38,45 @@ pub fn fuzzy_select_with_key<T, K, V>(
 where
     K: ToString,
 {
+    fuzzy_select_with_key_with_default(items, prompt, select_by, return_map, None)
+}
+
+pub fn fuzzy_select_with_key_with_default<T, K, V>(
+    items: Vec<T>,
+    prompt: impl Into<String>,
+    select_by: impl Fn(&T) -> K,
+    return_map: impl Fn(T) -> V,
+    default_index: Option<usize>,
+) -> anyhow::Result<Option<V>>
+where
+    K: ToString,
+{
+    // return `None` if we have nothing to select from
     if items.is_empty() {
         return Ok(None);
     }
 
+    // These are shown to the user, most likely some strings
     let items_keys = items.iter().map(select_by).collect::<Vec<_>>();
-    let selected_item = dialoguer::FuzzySelect::new()
-        .with_prompt(prompt)
-        .items(&items_keys)
-        .interact()
-        .map_err(anyhow::Error::from)
-        .and_then(|index| {
-            items
-                .into_iter()
-                .map(return_map)
-                .nth(index)
-                .context("Didn't find selected item")
-        })?;
+
+    // build stadard dialogue
+    let mut dialogue = dialoguer::FuzzySelect::new();
+    dialogue.with_prompt(prompt).items(&items_keys);
+
+    // optionally add default selection
+    if let Some(index) = default_index {
+        dialogue.default(index);
+    }
+
+    // select an item by index
+    let selected_item_index = dialogue.interact().map_err(anyhow::Error::from)?;
+
+    // transform the object with the index and only return the desired data
+    let selected_item = items
+        .into_iter()
+        .map(return_map)
+        .nth(selected_item_index)
+        .context("Didn't find selected item")?;
+
     Ok(Some(selected_item))
 }
