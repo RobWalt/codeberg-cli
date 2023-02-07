2df804784bdf19314cee7511cf742b01d6c58e83
RobWalt <robwalter96@gmail.com>
2023-02-07 20:22:08 +0100

    feat(types): impl `Display` for a bunch of types

--- crates/cod-types/src/api/label.rs
+++ crates/cod-types/src/api/label.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
@@ -6,4 +8,10 @@ pub struct Label {
     pub description: String,
     pub name: String,
     pub id: usize,
+}
+
+impl Display for Label {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
 }
