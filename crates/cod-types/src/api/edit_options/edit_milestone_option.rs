cc1408d8a5cfbcab6ace16031dc89084f7ec6119
RobWalt <robwalter96@gmail.com>
2023-02-09 18:36:58 +0100

    feat(datetimes): add datetimes to milestones

--- crates/cod-types/src/api/edit_options/edit_milestone_option.rs
+++ crates/cod-types/src/api/edit_options/edit_milestone_option.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::Serialize;
 
 use crate::api::milestone::Milestone;
@@ -8,14 +9,14 @@ pub struct EditMilestoneOption {
     pub description: Option<String>,
     pub state: Option<StateType>,
     pub title: Option<String>,
-    pub due_on: Option<String>,
+    pub due_on: Option<DateTime<Utc>>,
 }
 
 impl EditMilestoneOption {
     pub fn from_milestone(milestone: &Milestone) -> Self {
         Self {
             description: milestone.description.clone(),
-            due_on: milestone.due_on.clone(),
+            due_on: milestone.due_on,
             state: Some(milestone.state),
             title: Some(milestone.title.clone()),
         }
