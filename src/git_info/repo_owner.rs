67e25cdac2fb09ea11ab53c44ab173378f7a888e
RobWalt <robwalter96@gmail.com>
2023-03-13 05:34:08 +0100

    fix(git-info): fix failing test (+ more in details)
    - fixed failing test
    - refactored a bit of code to be more clean by passing functions to `map` calls directly instead of creating closures
    - documented that part of the codebase a bit

--- src/git_info/repo_owner.rs
+++ src/git_info/repo_owner.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::path::PathBuf;
 
 use anyhow::Context;
@@ -9,46 +10,66 @@ pub struct RepoAndOwner {
 }
 
 pub fn get_repo_owner() -> anyhow::Result<RepoAndOwner> {
+    // execute git command to get remotes
     std::process::Command::new("git")
         .arg("remote")
         .arg("-v")
         .output()
         .context("Couldn't detect git repository")
+        // convert cli output to string
         .map(|output| String::from_utf8_lossy(output.stdout.as_slice()).to_string())
+        // try to take first line
         .and_then(|output| {
             output
                 .lines()
                 .next()
-                .and_then(|repo| repo.split_whitespace().nth(1).map(|repo| repo.to_owned()))
+                .map(str::to_owned)
                 .context("Couldn't detect git repository")
         })
+        // try to take second word in line
+        .and_then(|repo| {
+            repo.split_whitespace()
+                .nth(1)
+                .map(str::to_owned)
+                .context("Couldn't detect git repository")
+        })
+        // convert to path type
         .map(PathBuf::from)
         .and_then(|mut git_path| {
+            // split away last part of path
             let repo = git_path
                 .file_name()
-                .map(|name| name.to_owned())
+                .map(OsStr::to_owned)
                 .context("Couldn't find repo name in git path")?;
             git_path.pop();
+            // split away second last part of path
             let owner = git_path
                 .file_name()
-                .map(|name| name.to_owned())
+                .map(OsStr::to_owned)
                 .context("Couldn't find repo owner in git path")?;
             Ok((repo, owner))
         })
         .and_then(|(repo, owner)| {
+            // cleanup + conversion to string
             let repo = repo
                 .to_str()
-                .map(|repo_name| repo_name.trim().to_owned())
+                .map(|repo_name| {
+                    // repo name could be something like myCoolRepo.git
+                    repo_name
+                        .chars()
+                        .take_while(alphanumeric_or_dash)
+                        .collect::<String>()
+                })
                 .context("Couldn't convert repo name into string")?;
             // owner needs additional cleanup since original url can be something like
             // git@codeberg.org:UserName/RepoName
             let owner = owner
                 .to_str()
-                .map(|repo_name| {
-                    repo_name
+                .map(|owner_name| {
+                    owner_name
                         .chars()
                         .rev()
-                        .take_while(char::is_ascii_alphanumeric)
+                        .take_while(alphanumeric_or_dash)
                         .collect::<String>()
                         .chars()
                         .rev()
@@ -57,6 +78,10 @@ pub fn get_repo_owner() -> anyhow::Result<RepoAndOwner> {
                 .context("Couldn't convert repo name into string")?;
             Ok(RepoAndOwner { repo, owner })
         })
+}
+
+fn alphanumeric_or_dash(char: &char) -> bool {
+    char.is_alphanumeric() || '-'.eq(char)
 }
 
 #[test]
